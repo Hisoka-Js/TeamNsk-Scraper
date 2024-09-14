8512ba2afa21d609fb566e6771039c31cf6faf99
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-09-14 13:49:16 -0600

    Actualizar

---
+++ run.js
@@ -0,0 +1,16 @@
+{
+  "name": "TeamNsk/Scraper",
+  "version": "2.0.0",
+  "description": "Wilsmac",
+  "main": "index.js",
+  "author": "",
+  "license": "ISC",
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/Wilsmac/TeamNsk-Scraper.git"
+  },
+  "bugs": {
+    "url": "https://github.com/Wilsmac/TeamNsk-Scraper/issues"
+  },
+  "homepage": "https://github.com/Wilsmac/TeamNsk-Scraper#readme"
+}
